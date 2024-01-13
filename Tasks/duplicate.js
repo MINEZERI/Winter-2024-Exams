97ed4d279773177d493f2947a7358515479646a1
MINEZERI <144104450+MINEZERI@users.noreply.github.com>
2024-01-13 22:32:33 +0200

    removed useless code blocks

--- Tasks/duplicate.js
+++ Tasks/duplicate.js
@@ -1,16 +1,13 @@
 // Return an array without duplicates
-// rename variables
+// removed unnecessary return and if
 
 'use strict';
 const duplicate = (string, number) => {
-  if (number <= 0) return [];
-  else {
     let res = [];
     for (let index = 0; index < number; index++) {
       res[index] = string;
     }
     return res;
-  }
 };
 
 module.exports = duplicate;
