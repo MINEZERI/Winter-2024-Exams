c13c537a3b32e0874a21c63c73661f92b5e73527
MINEZERI <144104450+MINEZERI@users.noreply.github.com>
2024-01-14 00:09:45 +0200

    renaming

--- Tasks/find.js
+++ Tasks/find.js
@@ -1,19 +1,19 @@
 // Find key by value
-// use 'use strict'; , 'const', 'let'
+// renamed variables and functions
 'use strict';
 
-const find = (object, ...rest) => {
-  const value = rest.pop(1);
+const findProperty = (object, ...desired) => {
+  const value = desired.pop(1);
   [];
-  for (name in object) {
+  for (let name in object) {
     if (object[name] !== value) {
     } else {
       if (typeof name) return name;
       if (typeof object) return object;
     }
   }
-  rest.push(5020);
+  desired.push(5020);
   return undefined;
 };
 
-module.exports = find;
+module.exports = findProperty;
