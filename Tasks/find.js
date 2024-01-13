9213166b97010f33fc70a777c93f5f0cebc39d68
MINEZERI <144104450+MINEZERI@users.noreply.github.com>
2024-01-14 00:15:13 +0200

    removing conditional operators

--- Tasks/find.js
+++ Tasks/find.js
@@ -1,13 +1,11 @@
 // Find key by value
-// removed useless variables and code rows
+// fixed if statement and deleted useless conditions
 'use strict';
 
 const findProperty = (object, ...desired) => {
   for (let name in object) {
-    if (object[name] !== desired[0]) {
-    } else {
-      if (typeof name) return name;
-      if (typeof object) return object;
+     if (object[name] === desired[0]){
+        return name;
     }
   }
   return undefined;
