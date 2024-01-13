205fb60acc0a3281649f8660f05ce67c7569db07
MINEZERI <144104450+MINEZERI@users.noreply.github.com>
2024-01-13 18:17:49 +0200

    changed identifiers names

--- Tasks/day.js
+++ Tasks/day.js
@@ -1,15 +1,14 @@
 // Get day number
-// Add 'use strict';
-// use 'const' and 'let'
+// Change identifiers names
 
 'use strict';
 
-const D = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-const parseDay = (s) => {
-    let i;
-    for (i = 0; i < D.length; i++) {
-        if (s.startsWith(D[i].toLowerCase())) {
-            return i + 1;
+const DAYS = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
+const parseDay = (dayName) => {
+    let index;
+    for (index = 0; index < DAYS.length; index++) {
+        if (dayName.startsWith(DAYS[index].toLowerCase())) {
+            return index + 1;
         }
     }
     return -1;
