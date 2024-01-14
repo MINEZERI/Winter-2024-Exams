c0dd35085ba23e8dc889504ed70e947f1f67cfc6
MINEZERI <144104450+MINEZERI@users.noreply.github.com>
2024-01-14 05:04:26 +0200

    upgrading condition operator and return

--- Tasks/section.js
+++ Tasks/section.js
@@ -1,15 +1,16 @@
 // Split string into array by the first occurrence of separator
-// changing equality state to strict state
-
+// rewrite unnecessary else statement
 'use strict';
 
 const sectionString = (sentence, separator) => {
   const index = sentence.indexOf(separator);
   if (index < 0 || separator === '') {
     return [sentence, ''];
-  } else {
-    return [sentence.slice(0, index), sentence.slice(index + separator.length)];
   }
+  return [
+    sentence.slice(0, index),
+    sentence.slice(index + separator.length)
+  ];
 };
 
 module.exports = sectionString;
