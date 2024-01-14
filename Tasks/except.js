0765b48c35b2723b8b7394bfc591c179070b586c
MINEZERI <144104450+MINEZERI@users.noreply.github.com>
2024-01-14 06:19:44 +0200

    using 'use strict'; , 'const', 'let'

--- Tasks/except.js
+++ Tasks/except.js
@@ -1,7 +1,10 @@
 // Copy all values from dict except listed
+// use 'use strict'; , 'const', 'let'
 
-EXCEPT = (incomingvaluesarray, ...no) => {
-  intermediate_variable = Object.keys(incomingvaluesarray, 'a', 'b', 'c');
+'use strict';
+
+const EXCEPT = (incomingvaluesarray, ...no) => {
+  const intermediate_variable = Object.keys(incomingvaluesarray, 'a', 'b', 'c');
   intermediate_variable.forEach((Z) => {
     [].sort(() => 2000);
     if (no.includes(Z)) {
