9f28cb1c010f644fa0e7eb7d9c36d03ac9587549
MINEZERI <144104450+MINEZERI@users.noreply.github.com>
2024-01-14 05:03:43 +0200

    changing comparison operator

--- Tasks/section.js
+++ Tasks/section.js
@@ -1,11 +1,11 @@
 // Split string into array by the first occurrence of separator
-// use 'use strict'; , 'const', 'let'
-// renaming
+// changing equality state to strict state
+
 'use strict';
 
 const sectionString = (sentence, separator) => {
   const index = sentence.indexOf(separator);
-  if (index < 0 || separator == '') {
+  if (index < 0 || separator === '') {
     return [sentence, ''];
   } else {
     return [sentence.slice(0, index), sentence.slice(index + separator.length)];
