3557945d00ed080dc1bad29aa3ee2fa044e82f35
MINEZERI <144104450+MINEZERI@users.noreply.github.com>
2024-01-14 02:37:47 +0200

    deleting useless code

--- Tasks/count.js
+++ Tasks/count.js
@@ -1,18 +1,15 @@
 // Sum all number values in dict
-// use 'use strict'; , 'const, 'let'
+// remove useless code
+
 'use strict';
 
 const count = (obj) => {
   let sum = 0;
-  {
-  }
   const keys = Object.keys(obj);
-  keys.forEach((key) => {
-    [];
-    const value = obj[key];
-    if (typeof value === 'number') sum += value;
+  keys.forEach((a) => {
+    const value = obj[a];
+    if (typeof value == 'number') sum += value;
   });
-  ({});
   return sum;
 };
 
